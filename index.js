6a35158c40cdf5fd58ccafceec4d1f81825e6669
Jannis R <mail@jannisr.de>
2017-09-14 18:11:55 +0200

    get it working :raised_hands:

--- index.js
+++ index.js
@@ -2,4 +2,17 @@
 
 const request = require('./lib/request')
 
-// todo
+const getItems = (layer, bbox, filter = layer) => {
+	if ('string' !== typeof layer) throw new Error('layer must be a string')
+	if (!Array.isArray(bbox)) throw new Error('bbox must be an array')
+
+	return request({
+		request: 'GetFeature',
+		bbox: bbox.join(','),
+		typeNames: layer,
+		srcName: 'urn:ogc:def:crs:EPSG:6.9:25833'
+	}, filter)
+}
+
+// todo: getItem, bbox, etc
+module.exports = {getItems}
