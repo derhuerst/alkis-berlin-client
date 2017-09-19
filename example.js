e3c5f35b3179d132a891986effb6bc20078cb18d
Jannis R <mail@jannisr.de>
2017-09-19 14:36:16 +0200

    re-introduce parser

--- example.js
+++ example.js
@@ -1,29 +1,12 @@
 'use strict'
 
 const {getItems} = require('.')
-const parsePolygon = require('./parse-polygon')
-
-const {findIn, textOf} = require('./lib/helpers')
-
-const parseStructure = (s) => {
-	const shp = findIn(s, 'fis:SHAPE')
-	return {
-		id: s.attributes && s.attributes['gml:id'] || null,
-		name: textOf(findIn(s, 'fis:Name')) || null,
-		category: {
-			id: textOf(findIn(s, 'fis:Bauwerksfunktion_schluessel')) || null,
-			name: textOf(findIn(s, 'fis:Bauwerksfunktion_bezeichnung')) || null
-		},
-		shape: shp && shp.children[0] && parsePolygon(shp.children[0]) || null
-	}
-}
 
 const alex = [392500, 5820000, 392000, 5820500]
 const someHouse = [393377, 5823461, 393127, 5823608]
 
 getItems('fis:s_wfs_alkis_bauwerkeflaechen', someHouse)
 .on('data', (structure) => {
-	structure = parseStructure(structure)
 	console.log(structure)
 })
 .on('error', console.error)
--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 'use strict'
 
 const request = require('./lib/request')
+const parseStructure = require('./lib/parse-structure')
 
-const getItems = (layer, bbox, filter = layer) => {
+const getItems = (layer, bbox, opt = {}) => {
 	if ('string' !== typeof layer) throw new Error('layer must be a string')
 	if (!Array.isArray(bbox)) throw new Error('bbox must be an array')
+
+	const filter = opt.filter || layer
+	if ('string' !== typeof filter) throw new Error('filter must be a string')
+	const parse = opt.parse || parseStructure
+	if ('function' !== typeof parse) throw new Error('parse must be a function')
 
 	return request({
 		request: 'GetFeature',
 		bbox: bbox.join(','),
 		typeNames: layer,
 		srcName: 'urn:ogc:def:crs:EPSG:6.9:25833'
-	}, filter)
+	}, filter, parse)
 }
 
 // todo: getItem, bbox, etc
