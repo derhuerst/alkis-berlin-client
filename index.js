979455e02ec1e75726a6c3e8178e274d0cd60e54
Jannis R <mail@jannisr.de>
2018-01-12 19:56:37 +0100

    use query-fis-broker-wfs

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 'use strict'
 
-const request = require('./lib/request')
+const getFeatures = require('query-fis-broker-wfs/get-features')
+const through = require('through2')
+
 const createParseStructure = require('./lib/parse-structure')
+
+// todo: https
+const endpoint = 'http://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_wfs_alkis_bauwerkeflaechen'
 
 const isObj = (v) => 'object' === typeof v && !Array.isArray(v)
 
@@ -9,20 +14,22 @@ const getItems = (layer, bbox, opt = {}) => {
 	if ('string' !== typeof layer) throw new Error('layer must be a string')
 	if (!Array.isArray(bbox)) throw new Error('bbox must be an array')
 
-	const filter = opt.filter || layer
-	if ('string' !== typeof filter) throw new Error('filter must be a string')
-
 	if (('fields' in opt) && !isObj(opt.fields)) {
 		throw new Error('opt.fields must be an object')
 	}
 	const parse = createParseStructure(opt.fields)
 
-	return request({
-		request: 'GetFeature',
-		bbox: bbox.join(','),
-		typeNames: layer,
-		srcName: 'urn:ogc:def:crs:EPSG:6.9:25833'
-	}, filter, parse)
+	return getFeatures(endpoint, layer, {
+		bbox,
+		crs: 'urn:ogc:def:crs:EPSG:6.9:25833'
+	})
+	.pipe(through.obj((feature, _, cb) => {
+		try {
+			cb(null, parse(feature))
+		} catch (err) {
+			cb(err)
+		}
+	}))
 }
 
 // todo: getItem, bbox, etc
