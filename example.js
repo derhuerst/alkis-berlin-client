3f59f2393b356228fc184fd9a9a3a2018db7c773
Jannis R <mail@jannisr.de>
2018-09-11 00:16:29 +0200

    drop Node 7 :green_heart:, 1.0.5

--- example.js
+++ example.js
@@ -3,6 +3,7 @@
 const {getItems} = require('.')
 
 const alexanderplatz = [392500, 5820000, 392000, 5820500]
+// const wedding = [388995,5822577,389287,5822750]
 
 getItems('fis:s_wfs_alkis_bauwerkeflaechen', alexanderplatz)
 .on('data', (structure) => {
